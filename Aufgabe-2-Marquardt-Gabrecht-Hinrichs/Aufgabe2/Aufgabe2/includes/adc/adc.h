cac95e3ca31426c8c35aa1993a62b617bb52523b
Matthias Hinrichs <matthias.hinrichs@hotmail.de>
2017-12-12 00:08:05 +0100

    includepath, adc_init() und adc_read()
    Die zwei Funktionen für die AD-Wandlung und die relativen Pfade zu den .h-Dateien hinzugefügt.

--- Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/includes/adc/adc.h
+++ Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/includes/adc/adc.h
@@ -3,14 +3,21 @@
  *
  * Created: 11.12.2017 22:10:13
  *  Author: Matthias Hinrichs
- */ 
-
-
+ */
 #ifndef ADC_H_
 #define ADC_H_
 
+#include <avr/io.h>
+#include <inttypes.h>
 
+/************************************************************************/
+/* Initialisierung des AD-Wandlers                                      */
+/************************************************************************/
+void adc_init(void);
 
-
+/************************************************************************/
+/* Auslesen der AD-Wandlung												*/
+/************************************************************************/
+uint8_t adc_read(uint8_t channel);
 
 #endif /* ADC_H_ */
