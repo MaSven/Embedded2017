7d316e07aef5a28964d46ebdeb7764fe643497e8
Eiamnacken <Sven2q@hotmail.de>
2017-12-11 15:24:49 +0100

    init aufgabe 2

---
+++ Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/Aufgabe2.c
@@ -0,0 +1,17 @@
+/*
+ * Aufgabe2.c
+ *
+ * Created: 11.12.2017 15:24:28
+ *  Author: 240747
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
