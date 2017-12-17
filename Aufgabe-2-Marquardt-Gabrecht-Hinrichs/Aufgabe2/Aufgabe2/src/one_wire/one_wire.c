ac35a9753fb14282d042b4f66057447fa8a97a2c
Sven Marquardt <sven2q@hotmail.de>
2017-12-17 17:22:42 +0100

    Reset für 1-Wire hinzu gefügt

--- Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/src/one_wire/one_wire.c
+++ Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/src/one_wire/one_wire.c
@@ -4,5 +4,26 @@
  * Created: 11.12.2017 22:11:02
  *  Author: Matthias Hinrichs
  */ 
+
 #include "one_wire/one_wire.h"
-#include <conio.h>
+#include <avr/io.h>
+#include  <avr/cpufunc.h>
+#include <util/delay.h>
+#define TEMPERATURE_REGISTER DDRB
+#define TEMPERATURE_PORT PORTB
+
+void one_wire_deactivate(){
+	TEMPERATURE_REGISTER = (1<<DDB2);
+	TEMPERATURE_PORT = (0<<PORTB2);
+	_NOP();
+}
+
+
+void reset(){
+	TEMPERATURE_PORT = (1<<PORTB2);
+	TEMPERATURE_PORT = (0<<PORTB2);
+	_delay_us(480);
+	TEMPERATURE_PORT = (1<<PORTB2);
+	_delay_us(70);
+	uint8_t read_slave_count = PORTB;
+}
