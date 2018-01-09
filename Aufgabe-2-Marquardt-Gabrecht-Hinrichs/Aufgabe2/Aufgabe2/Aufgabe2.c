ca161e1e6ca83e0d807ec791d7adfefcf7eee169
Eiamnacken <Sven2q@hotmail.de>
2018-01-09 14:52:47 +0100

    Testfunktion angefügt mit laufendem code

--- Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/Aufgabe2.c
+++ Aufgabe-2-Marquardt-Gabrecht-Hinrichs/Aufgabe2/Aufgabe2/Aufgabe2.c
@@ -1,9 +1,9 @@
 /*
- * Aufgabe2.c
- *
- * Created: 11.12.2017 22:06:28
- *  Author: Matthias Hinrichs
- */ 
+* Aufgabe2.c
+*
+* Created: 11.12.2017 22:06:28
+*  Author: Matthias Hinrichs
+*/
 
 
 #include <avr/io.h>
@@ -14,14 +14,17 @@
 
 int main(void)
 {
+	LCDDDR = 0xFF;
+	LCDPORT = 0x00;
 	lcd_init();
 	adc_init();
-    while(1)
-    {
-          
-		  uint8_t adc = adc_read();
-		  char adc_string[4];
-		  itoa(adc,adc_string,10);
-		  lcd_send_string("Hallo Welt");
-    }
+	while(1)
+	{
+		_delay_ms(500);
+		lcd_clear();
+		uint8_t adc = adc_read();
+		char adc_string[4];
+		itoa(adc,adc_string,10);
+		lcd_send_string(adc_string);
+	}
 }
